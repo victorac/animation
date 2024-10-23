4762c1fd1e53412d1ee068df06761d5114a2b3b0
victorac <victorde.ac@gmail.com>
2024-10-23 02:48:41 -0300

    initial animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,131 @@
-import Image from "next/image";
+"use client";
+import anime from "animejs/lib/anime.es.js";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const containerRef = useRef<HTMLDivElement>(null);
+  const svgRef = useRef<SVGSVGElement>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!containerRef.current || !svgRef.current) return;
+
+      const containerWidth = containerRef.current.offsetWidth;
+      const svgWidth = svgRef.current.getBoundingClientRect().width;
+      const svgHeight = svgRef.current.getBoundingClientRect().height;
+
+      const rightPosition = containerWidth - svgWidth / 2;
+      const leftPosition = -(containerWidth / 2 - svgWidth / 2);
+
+      // Center the SVG precisely
+      anime.set(svgRef.current, {
+        translateY: `-${svgHeight / 2}px`,
+        left: 0,
+        top: "50%",
+      });
+
+      const xTime = 2000;
+
+      var timeline = anime.timeline({
+        easing: "easeInOutSine",
+        loop: true,
+      });
+
+      // Path morphing animation when reaching the left position
+      timeline.add(
+        {
+          targets: "path",
+          d: [
+            "M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z",
+            "M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 25 38.8071 25 25C25 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z",
+          ],
+          duration: xTime * 0.2,
+        },
+        0
+      );
+
+      // Move to the right
+      timeline.add(
+        {
+          targets: svgRef.current,
+          translateX: [
+            { value: 0, duration: 0 },
+            { value: `${rightPosition - svgWidth / 2}px`, duration: xTime },
+          ],
+        },
+        0
+      );
+
+      // Path morphing animation when reaching the right position
+      timeline.add(
+        {
+          targets: "path",
+          d: [
+            "M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 25 38.8071 25 25C25 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z",
+            "M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z",
+          ],
+          duration: xTime * 0.1,
+        },
+        "-=150"
+      );
+
+      // Path morphing animation when reaching the right position
+      timeline.add({
+        targets: "path",
+        d: [
+          "M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z",
+          "M25 25C25 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 25 11.1929 25 25Z",
+        ],
+        duration: xTime * 0.2,
+      });
+
+      // Move to the left
+      timeline.add(
+        {
+          targets: svgRef.current,
+          translateX: [
+            { value: `${rightPosition - svgWidth / 2}px`, duration: 0 },
+            { value: 0, duration: xTime },
+          ],
+        },
+        `-=${xTime * 0.2}`
+      );
+
+      // Path morphing animation when reaching the right position
+      timeline.add(
+        {
+          targets: "path",
+          d: [
+            "M25 25C25 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 25 11.1929 25 25Z",
+            "M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z",
+          ],
+          duration: xTime * 0.1,
+        },
+        "-=150"
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <div ref={containerRef} className="relative h-screen w-full">
+      <svg
+        ref={svgRef}
+        width="50"
+        height="50"
+        viewBox="0 0 50 50"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute left-0 top-1/2 -translate-y-1/2"
+      >
+        <path
+          d="M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z"
+          fill="#D9D9D9"
+        />
+      </svg>
     </div>
   );
 }
